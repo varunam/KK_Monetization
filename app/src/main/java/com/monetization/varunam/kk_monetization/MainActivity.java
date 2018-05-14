490f24a88f2e9579ac1a495962ba863f51faf309
Varun A M <varun.am@C02QD5QQG8WP.local>
2018-05-14 22:32:24 +0530

    initial skeleton

--- app/src/main/java/com/monetization/varunam/kk_monetization/MainActivity.java
+++ app/src/main/java/com/monetization/varunam/kk_monetization/MainActivity.java
@@ -1,13 +1,33 @@
 package com.monetization.varunam.kk_monetization;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button iwt_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iwt_button = findViewById(R.id.iwtButtonID);
+        iwt_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.iwtButtonID:
+                Toast.makeText(getApplicationContext(), "Button", Toast.LENGTH_LONG).show();
+                break;
+            default: break;
+        }
     }
 }
